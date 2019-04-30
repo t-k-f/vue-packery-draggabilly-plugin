0a47f9452ff4da05dfa1adf4296cae095a48bb91
Sven Parker <sven@trustcruit.com>
2019-04-30 08:44:25 +0200

    Added support for empty options object, thanks @tof06

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 
 import Vue from 'vue'
 import Draggabilly from 'draggabilly'
-import {packeryEvents} from 'vue-packery-plugin'
+import { packeryEvents } from 'vue-packery-plugin'
 
 const draggabillyPlugin = () => {}
 
 export default draggabillyPlugin
 
-function handlerFactory(el) {
+function recalcFactory(el) {
     return ( event, pointer, moveVector ) => {
         el.draggie.setPosition( el.draggie.position.x, el.draggie.position.y );
     };
@@ -18,24 +18,26 @@ draggabillyPlugin.install = function (Vue, options)
 {
     Vue.directive('draggabilly', {
         inserted (el, binding) {
-            el.draggie = el.draggie || new Draggabilly(el, binding.value)
+            const options = binding.value || {}
+            el.draggie = el.draggie || new Draggabilly(el, options)
 
             packeryEvents.$emit('draggie', {draggie: el.draggie, node: el.parentNode})
 
-            if (binding.value.disableDraggabilly) {
+            if (options.disableDraggabilly) {
                 el.draggie.disable()
             }
             else {
                 el.draggie.enable()
             }
         },
-        update (el, { value }) {
-            if(el.handlerFunc){
-                el.draggie.off( 'dragMove', el.handlerFunc);
+        update (el, binding) {
+            const options = binding.value || {}
+            if(el.recalcFunc){
+                el.draggie.off( 'dragMove', el.recalcFunc);
             }
-            if (value.recalculateOnMove){
-                el.handlerFunc = handlerFactory(el);
-                el.draggie.on( 'dragMove', el.handlerFunc);
+            if (options.recalculateOnMove){
+                el.recalcFunc = recalcFactory(el);
+                el.draggie.on( 'dragMove', el.recalcFunc);
             }
         },
         unbind (el) {
